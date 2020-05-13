fb4d6095e3429140338047f126e66765caeee8bd
Eli Oxman <eli.oxman@gmail.com>
2020-05-13 22:56:52 +0300

    Add requirements directly to setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
 from distutils.core import setup
-try:
-      from pip.req import parse_requirements
-except ImportError:
-      from pip._internal.req import parse_requirements
-
-install_reqs = parse_requirements("requirements.txt", session=False)
-
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='alooma',
-    version='0.4.15',
+    version='0.4.16',
     description='Alooma python API',
     author='Alooma',
     author_email='support@alooma.io',
     packages=['alooma'],
-    install_requires=reqs,
+    install_requires=[
+      "urllib3>=1.13",
+      "requests>=2.9.0",
+      "six>=1.4.1"
+    ],
     keywords=['alooma', 'api']
 )
